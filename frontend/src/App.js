78ba4e82800153bf6e2eb2aacbe0818c92b8099c
IT21108990 <IT21108990@my.sliit.lk>
2023-05-03 18:29:19 +0530

    created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import LoyaltyDB from './pages/loyalty/loyaltydb'
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <BrowserRouter>
+      <Routes>
+        <Route path="/loyalty-reward" element={<LoyaltyDB />} />
+
+
+        
+      </Routes>  
+      </BrowserRouter>
     </div>
   );
 }
