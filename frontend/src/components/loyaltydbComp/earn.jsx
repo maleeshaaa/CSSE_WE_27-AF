892fc5b6bc1f899b8b5251769e2100b6e9a47279
IT21108990 <IT21108990@my.sliit.lk>
2023-05-04 17:28:44 +0530

    earn page

---
+++ frontend/src/components/loyaltydbComp/earn.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/esm/Button";
+
+export default function earn() {
+    
+
+  return (
+    <div styel={{display: "flex"}}>
+      <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Header>Feedback</Card.Header>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            Your feedback is vital in shaping unforgettable experiences for
+            future travelers. Share your valuable insights and help us create
+            enchanting adventures that exceed expectations!<br /><br />
+            <Button>Give Feedback</Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+      </div>
+
+      {/* Blog */}
+      <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Header>Blog</Card.Header>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            Your blog is vital in shaping unforgettable experiences for
+            future travelers. Share your valuable insights and help us create
+            enchanting adventures that exceed expectations!<br /><br />
+            <Button>Post a Blog</Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+      </div>
+    </div>
+  );
+}
