e6636b616db9888d27acb65a587c61e375d94e34
IT21108990 <IT21108990@my.sliit.lk>
2023-05-04 15:52:53 +0530

    redeem page

--- frontend/src/pages/loyalty/loyaltydb.jsx
+++ frontend/src/pages/loyalty/loyaltydb.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import Navbar from "../../components/navbar.jsx";
+import Reward from "../../images/reward.gif";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Redeem from "../../components/loyaltydbComp/redeem"
+import "./styles.css";
 
 export default function LoyaltyDB() {
+ 
   return (
     <div>
       <Navbar />
-
+      <div className="main_container">
+        <div>
+          <h2 className="person_name">Hi Amal</h2>
+        </div>
+        <div>
+          <img className="rewardgif" src={Reward} alt="" />
+          <h5 className="rewardtext">Available Points: 100</h5>
+        </div>
+      </div>
+      <div className="tab_div" >
+        <Tabs
+          defaultActiveKey="profile"
+          id="fill-tab-example"
+          className="mb-3 tab_div"
+          fill
+        >
+          <Tab eventKey="earn" title="Earn"></Tab>
+          <Tab eventKey="redeem" title="Redeem">
+            <Redeem/>
+          </Tab>
+          <Tab eventKey="status" title="Status"></Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
