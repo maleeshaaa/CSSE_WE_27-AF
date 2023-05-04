d9940602207ec68da52dc846ccdfb5ba6443d31a
azaynm <maleeshaashenjvc@gmail.com>
2023-05-04 10:53:56 +0530

    made changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import LoyaltyDB from './pages/loyalty/loyaltydb'
 import TravelPlan from './pages/travel-plan/travel_plan';
+import MyProfile from './pages/package/MyProfile';
+import SinglePackage from './pages/package/SinglePackage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/loyalty-reward" element={<LoyaltyDB />} />
           <Route path="/travel-plan" element={<TravelPlan />} />
+          <Route path="/profile" element={<MyProfile />} />
+          <Route path="/package/:id" element={<SinglePackage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/package/PackageCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const PackageCard = ({ item }) => {
+    const navigate = useNavigate();
+    
+    return (
+        <div class="card" style={{width: '18rem'}}>
+            <div class="card-body">
+                <h5 class="card-title">{item.packageName}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{item.packagePrice}</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="button" class="btn btn-dark" style={{ width:'200px' }} onClick={()=>navigate(`/package/${item.id}`)}>Pick Package</button>
+                
+            </div>
+        </div>
+    )
+}
+
+export default PackageCard
