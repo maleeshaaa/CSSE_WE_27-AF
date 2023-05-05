6cc0dfa16a5c2984f1941108e2904498c267bddd
azaynm <maleeshaashenjvc@gmail.com>
2023-05-05 15:53:24 +0530

    added login, register backend

---
+++ backend/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const auth = async (req, res, next) => {
+    const token = req.header("x-access-token");
+    if (!token)
+        return res
+            .status(403)
+            .json({ error: true, message: "Access Denied: No token provided" });
+
+    try {
+        const tokenDetails = jwt.verify(
+            token,
+            process.env.ACCESS_TOKEN_PRIVATE_KEY
+        );
+        req.user = tokenDetails;
+        next();
+    } catch (err) {
+        res.status(403)
+            .json({ error: true, message: "Access Denied: Invalid Token" });
+    }
+};
+
+module.exports = auth;
