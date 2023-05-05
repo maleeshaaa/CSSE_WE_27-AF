bc59939b113fbb8692fe9aad58b149dd3e99a1d9
Maleesha Shashindi <maleeshas.2000@gmail.com>
2023-05-05 16:47:57 +0530

    Update Frontend

--- frontend/src/pages/payment/payment.jsx
+++ frontend/src/pages/payment/payment.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from "../../components/navbar.jsx";
 import Footer from "../../components/Home/footer.jsx";
-import "./payment-style.css"
+import Payment from "../../components/Payment/package-payment.jsx"
+import "./payment.css"
 
 const payment = () => {
   return (
     <div>
       <Navbar />
-      <div className="container-payment">Payment</div>
+      <div className="container-payment">
+        {/* Payment Form */}
+        <div className="payment-form">
+          <Payment/>
+        </div>
+      </div>
       <Footer />
     </div>
   );
