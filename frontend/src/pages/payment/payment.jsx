9545cb48ba41744078aae30dd0cf7fa1d1f1adf7
Maleesha Shashindi <maleeshas.2000@gmail.com>
2023-05-07 16:51:50 +0530

    Create Add Places Page

--- frontend/src/pages/payment/payment.jsx
+++ frontend/src/pages/payment/payment.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import Footer from "../../components/Home/footer.jsx";
-import Payment from "../../components/Payment/package-payment.jsx"
-import "./payment.css"
+import Payment from "../../components/Payment/package-payment.jsx";
+import "./payment.css";
 
 const payment = () => {
   return (
@@ -56,25 +56,29 @@ const payment = () => {
                   </Card.Subtitle>
                   <p
                     style={{
-                      fontSize: "0.9rem",
+                      fontSize: "0.85rem",
                       fontFamily: "Lucida Sans",
                     }}
                   >
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content. Some quick example text
-                    to build on the card title and make up the bulk of the
-                    card's content. Some quick example text to build on the card
-                    title and make up the bulk of the card's content. Some quick
-                    example text to build on the card title and make up the bulk
-                    of the card's content. Some quick example text to build on
-                    the card title and make up the bulk of the card's content.
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content. Some quick example text
-                    to build on the card title and make up the bulk of the
-                    card's content.Some quick example text to build on the card
-                    title and make up the bulk of the card's content.Some quick
-                    example text to build on the card title and make up the bulk
-                    of the card's content.
+                    We are thrilled to present you with a tailor-made travel
+                    package that has been created exclusively for your request.
+                    This five-day adventure will take you through the scenic and
+                    historic provinces of Central Sri Lanka, specifically Kandy
+                    and Matale. These districts are rich with cultural and
+                    natural attractions that will leave you in awe. The journey
+                    begins on May 15th, 2023, and will take you on a captivating
+                    journey through the heart of Sri Lanka. You will visit
+                    world-famous destinations like the Temple of the Tooth in
+                    Kandy, a UNESCO World Heritage Site, Matale Alu Viharaya and
+                    explore the scenic beauty of the Knuckles Mountain Range,
+                    Sembuwaththa Lake, Hunnas Falls and Mandaram Nuwara. You
+                    will also get to experience Sri Lanka's lush tea
+                    plantations, spice gardens, and botanical gardens. This
+                    travel package has been designed to provide you with an
+                    unforgettable experience, giving you a glimpse into Sri
+                    Lanka's vibrant culture and natural beauty. We hope you
+                    enjoy your journey with us and create memories that will
+                    last a lifetime.
                   </p>
                 </Card.Body>
               </Card>
@@ -85,6 +89,6 @@ const payment = () => {
       <Footer />
     </div>
   );
-}
+};
 
-export default payment
+export default payment;
