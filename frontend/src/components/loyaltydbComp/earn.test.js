54635e02a61deec04778fa93147cb5916c15a372
IT21108990 <IT21108990@my.sliit.lk>
2023-05-07 23:52:27 +0530

    jest dependency

---
+++ frontend/src/components/loyaltydbComp/earn.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Redeem from './redeem';
+
+// test('renders MyComponent', () => {
+//     const { getByText  } = render(<Redeem />);
+//     const linkElement = getByText ("http://localhost:8080/vouchers/id");
+//     expect(linkElement).toBeInTheDocument();
+//   });
