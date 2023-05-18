04e6ced7db3376d7b459c3f6a123df060a5e1981
IT21108990 <IT21108990@my.sliit.lk>
2023-05-18 20:52:21 +0530

    donation page done, decrease route added

--- backend/routes/blog/blog-route.js
+++ backend/routes/blog/blog-route.js
@@ -60,4 +60,29 @@ router.route("/:id").get((req, res) => {
     }
 );
 
+router.post('/decrease-points', async (req, res) => {
+  try {
+    const username = req.body.username;
+    const pointsToDecrease = req.body.points;
+
+    const user = await User.findOne({ userName: username });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    if (user.userPoints < pointsToDecrease) {
+      return res.status(400).json({ error: 'Insufficient points.' });
+    }
+
+    user.userPoints -= pointsToDecrease;
+    await user.save();
+
+    res.json({ message: 'Points decreased successfully.' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'An error occurred while decreasing points.' });
+  }
+});
+
+
 export default router;
